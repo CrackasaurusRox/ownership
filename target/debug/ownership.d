c5babe05ec90237009526dda1473f018548f71b7
Anima7ron <george@forzaesotico.com>
2021-03-05 00:03:39 -0600

    complete

---
+++ target/debug/ownership.d
@@ -0,0 +1,1 @@
+/home/anima7ron/rust/projects/ownership/target/debug/ownership: /home/anima7ron/rust/projects/ownership/src/main.rs
---
+++ target/rls/debug/deps/ownership-167bc4e92ac07439.d
@@ -0,0 +1,5 @@
+/home/anima7ron/rust/projects/ownership/target/rls/debug/deps/ownership-167bc4e92ac07439.rmeta: src/main.rs
+
+/home/anima7ron/rust/projects/ownership/target/rls/debug/deps/ownership-167bc4e92ac07439.d: src/main.rs
+
+src/main.rs:
